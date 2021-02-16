7df5a5c6e7a28011a3e35f143364f7ea2870a346
Flobby <2541226493@qq.com>
2021-02-16 13:04:06 +0800

    操作歌单数据库进行查改删操作接口

--- controller/playlist.js
+++ controller/playlist.js
@@ -4,6 +4,7 @@ const Router = require('koa-router');
 const router = new Router()
 // 引入封装的调用云函数模块
 const callCloudFn = require('../utils/callCloudFn');
+const callCloudDB = require('../utils/callCloudDB');
 
 // 歌单模块，获取歌单列表接口
 router.get('/list', async (ctx, next) => {
@@ -26,6 +27,44 @@ router.get('/list', async (ctx, next) => {
     ctx.body = {
         data,
         code:20000,
+    }
+})
+
+// 根据id查询歌单详情，通过封装的操作云数据库函数实现，注意传的id是云数据库的唯一_id
+router.get('/detail', async(ctx, next) => {
+    console.log(ctx.request.query.id)
+    const query =  `db.collection('playlist').doc('${ctx.request.query.id}').get()`
+    const res = await callCloudDB(ctx, 'databasequery', query)
+    ctx.body = {
+        code: 20000,
+        data: JSON.parse(res.data)
+    }
+})
+
+// 更新歌单（名称和描述），传参为请求体json对象
+router.post('/update', async(ctx, next) => {
+    const params = ctx.request.body
+    const query =  `db.collection('playlist').doc('${params._id}').update({
+        data: {
+            name: '${params.name}',
+            description: '${params.description}'
+        }
+    })`
+    const res = await callCloudDB(ctx, 'databaseupdate', query)
+    ctx.body = {
+        code: 20000,
+        data: res
+    }
+})
+
+// 删除歌单
+router.post('/delete', async(ctx, next) => {
+    const params = ctx.request.query
+    const query =  `db.collection('blog').doc('${params.id}').remove()`
+    const res = await callCloudDB(ctx, 'databasedelete', query)
+    ctx.body = {
+        code: 20000,
+        data: res
     }
 })
 
